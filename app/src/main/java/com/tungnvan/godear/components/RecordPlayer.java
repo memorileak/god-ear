d5d0ec6fb65578d6a117bb5205c1fb4bed4c6b82
TungNguyen <tungnvan.mar12th@gmail.com>
2019-05-19 15:14:49 +0700

    Add play and pause for player

--- app/src/main/java/com/tungnvan/godear/components/RecordPlayer.java
+++ app/src/main/java/com/tungnvan/godear/components/RecordPlayer.java
@@ -5,14 +5,14 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.tungnvan.godear.R;
 import com.tungnvan.godear.controllers.Player;
+import com.tungnvan.godear.utils.RecordNameUtils;
 import com.tungnvan.godear.utils.TimeUtils;
-
-import java.io.File;
 
 public class RecordPlayer {
 
@@ -22,24 +22,25 @@ public class RecordPlayer {
     private AlertDialog.Builder dialog_builder;
     private AlertDialog dialog;
     private View record_player_body;
+    private Button record_player_control_button;
+    private TextView record_player_record_name;
     private SeekBar record_player_seekbar;
     private TextView current_time;
     private TextView end_time;
-    private File file_to_play;
     private Player player;
 
     public RecordPlayer(Context context, String file_path) {
         own_context = context;
-        file_to_play = new File(file_path);
         preparePlayer(context, file_path);
         record_player_body = LayoutInflater.from(own_context).inflate(R.layout.record_player_body, null);
+        record_player_control_button = (Button) record_player_body.findViewById(R.id.record_player_control_button);
+        record_player_record_name = (TextView) record_player_body.findViewById(R.id.record_player_record_name);
         record_player_seekbar = (SeekBar) record_player_body.findViewById(R.id.record_player_seekbar);
         current_time = (TextView) record_player_body.findViewById(R.id.record_player_current_time);
         end_time = (TextView) record_player_body.findViewById(R.id.record_player_end_time);
-        setInitialValues();
+        setInitialValues(context, file_path);
         dialog_builder = new AlertDialog.Builder(own_context);
         dialog = dialog_builder
-                .setTitle(file_to_play.getName())
                 .setView(record_player_body)
                 .create();
         dialog.setCanceledOnTouchOutside(false);
@@ -68,7 +69,15 @@ public class RecordPlayer {
         player.prepare();
     }
 
-    private void setInitialValues() {
+    private void setInitialValues(Context context, String file_path) {
+        record_player_control_button.setBackground(context.getResources().getDrawable(R.drawable.player_pause_button));
+        record_player_control_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleControlButtonClick();
+            }
+        });
+        record_player_record_name.setText(RecordNameUtils.produceFileNameFromPath(file_path));
         current_time.setText(TimeUtils.toHMSString(0));
         end_time.setText(TimeUtils.toHMSString(player.getDuration() / 1000));
         record_player_seekbar.setMax(player.getDuration());
@@ -87,13 +96,30 @@ public class RecordPlayer {
         });
     }
 
+    private void updateControlButtonUIByPlayerState(boolean is_playing) {
+        record_player_control_button.setBackground(
+            is_playing
+                ? own_context.getResources().getDrawable(R.drawable.player_pause_button)
+                : own_context.getResources().getDrawable(R.drawable.player_play_button)
+        );
+    }
+
     private void updateDialogUIByPlayerState() {
+        updateControlButtonUIByPlayerState(player.isPlaying());
         current_time.setText(TimeUtils.toHMSString(player.getCurrentPosition() / 1000));
         record_player_seekbar.setProgress(player.getCurrentPosition());
     }
 
     private void updateSeekbarByPlayerPosition() {
         record_player_seekbar.setProgress(player.getCurrentPosition());
+    }
+
+    private void handleControlButtonClick() {
+        if (player.isPlaying()) {
+            player.pause();
+        } else {
+            player.start();
+        }
     }
 
     public void showDialog() {
